dbe8ace4effbe9a91309dd5da0eff1820eb511b2
Nguyen-Anh-Duong <nguyenanhduong112004@gmail.com>
2025-05-11 01:02:42 +0700

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header" bg-color="primary">
+        <h1>My Vocabulary App</h1>
+        <p>Welcome to the My Vocabulary App!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
