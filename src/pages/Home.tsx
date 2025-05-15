66196ed9b8f385abe95b4bc86f3e03e4133f6b38
Nguyen-Anh-Duong <nguyenanhduong112004@gmail.com>
2025-05-16 06:07:51 +0700

    add initial page components

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to My Vocabulary App</h1>
+      <p>Here you can learn and practice new words.</p>
+    </div>
+  );
+}
+
+export default Home;
